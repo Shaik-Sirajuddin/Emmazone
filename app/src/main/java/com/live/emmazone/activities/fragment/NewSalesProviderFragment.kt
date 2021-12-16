9c1f73a78cb403f9b8919dd1ece13cc0331ea2ea
Nitish <nitish.kumar@cqlsys.co.uk>
2021-12-16 15:52:50 +0530

    code merged

--- app/src/main/java/com/live/emmazone/activities/fragment/NewSalesProviderFragment.kt
+++ app/src/main/java/com/live/emmazone/activities/fragment/NewSalesProviderFragment.kt
@@ -1,6 +1,5 @@
 package com.live.emmazone.activities.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.live.emmazone.R
-import com.live.emmazone.activities.Interface.OnItemClick
-import com.live.emmazone.activities.provider.OrderDetailNewSaleActivity
 import com.live.emmazone.adapter.AdapterProviderNewSales
 import com.live.emmazone.model.ModelOnGoingOrders
 import com.live.emmazone.model.ModelProviderNewSale
@@ -22,7 +19,8 @@ class NewSalesProviderFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         val view: View = LayoutInflater.from(context)
             .inflate(R.layout.fragment_new_sales_provider, container, false)
 
@@ -32,15 +30,32 @@ class NewSalesProviderFragment : Fragment() {
 
         val listChildRecycler = ArrayList<ModelOnGoingOrders>()
 
-        listChildRecycler.add(ModelOnGoingOrders(R.drawable.shoes_square, "Brend Shoe",
-            "03", "90.00€"))
-        listChildRecycler.add(ModelOnGoingOrders(R.drawable.shoes_square, "Brend Shoe",
-            "03", "90.00€"))
+        listChildRecycler.add(
+            ModelOnGoingOrders(
+                R.drawable.shoes_square, "Brend Shoe",
+                "03", "90.00€"
+            )
+        )
+        listChildRecycler.add(
+            ModelOnGoingOrders(
+                R.drawable.shoes_square, "Brend Shoe",
+                "03", "90.00€"
+            )
+        )
 
         list.add(
-            ModelProviderNewSale("Order ID:", "PLU9540572", R.drawable.avtarr_girl, "Allen Chandler",
-                "Delivery Type", " Home Delivery", listChildRecycler, R.drawable.pendding_g, "29-march-2021"
-                ,status = "pending")
+            ModelProviderNewSale(
+                "Order ID:",
+                "PLU9540572",
+                R.drawable.avtarr_girl,
+                "Allen Chandler",
+                "Delivery Type",
+                " Home Delivery",
+                listChildRecycler,
+                R.drawable.pendding_g,
+                "29-march-2021",
+                status = "pending"
+            )
         )
 
         val items = ArrayList<ModelOnGoingOrders>()
@@ -50,9 +65,11 @@ class NewSalesProviderFragment : Fragment() {
         )
 
         list.add(
-            ModelProviderNewSale("Order ID:", "PLU9540572", R.drawable.avtarr_girl, "Allen Chandler",
+            ModelProviderNewSale(
+                "Order ID:", "PLU9540572", R.drawable.avtarr_girl, "Allen Chandler",
                 "Delivery Type", " Home Delivery", items, R.drawable.pendding_g, "29-march-2021",
-                status = "pending")
+                status = "pending"
+            )
         )
 
         rv.adapter = AdapterProviderNewSales(requireContext(), list)
